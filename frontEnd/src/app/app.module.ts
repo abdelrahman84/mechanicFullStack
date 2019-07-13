765e2b10f692b6e704c723091f38e3329964b0eb
abdelrahman84 <abdelrahman.farag84@gmail.com>
2019-07-14 00:23:56 +0200

    configuring login front end logic

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { PasswordComponent } from './components/password/password.component';
 import { RequestResestComponent } from './components/password/request-resest/request-resest.component';
 import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { ResponseResetComponent } from './components/password/response-reset/res
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
